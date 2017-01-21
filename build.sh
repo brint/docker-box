a61f47300d9e89b0e311ef766145ec57e80c0060
Brint O'Hearn <brint.ohearn@rackspace.com>
2017-01-21 12:15:13 -0600

    Adding '--rm' flag to tests to clean up after self

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -ex
 docker build -t brint/docker-box .
 
 # Test
-docker run brint/docker-box docker
-docker run brint/docker-box docker-machine --version
-docker run brint/docker-box docker-compose version
+docker run --rm brint/docker-box docker
+docker run --rm brint/docker-box docker-machine --version
+docker run --rm brint/docker-box docker-compose version
